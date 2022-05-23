41165a4439cde147451ce23d0fc16fc6a9653261
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-05-24 03:44:50 +0800

    all product api done

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nwui0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zczar.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
+    try {
+        await client.connect();
+        const productCollection = client.db('manufacturePart').collection('products');
 
+        // api for loading all products
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        });
     }
-    finally{
+    finally {
 
     }
 }
