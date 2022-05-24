ce2553dc88b9287477f51b8b64ee116891f91cce
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-05-24 14:48:02 +0800

    search by id api done

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ async function run() {
             const product = await cursor.toArray();
             res.send(product);
         });
+
+        // get products by id 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
     }
     finally {
 
