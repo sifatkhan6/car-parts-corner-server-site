ae977ee0b5aad61dfbf7e258fd8b557376aec8e8
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-05-24 14:42:10 +0800

    jwt done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const res = require('express/lib/response');
+const query = require('express/lib/middleware/query');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -12,6 +14,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zczar.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            console.log(err);
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
